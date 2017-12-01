bcfd0be5788c4c3036b91549504ade58c6a1d2e0
Chris Hall <chall@selectrehab.com>
2017-12-01 13:56:22 -0600

    First pass at a make_pgpass task

--- lib/db/clone/rake_task.rb
+++ lib/db/clone/rake_task.rb
@@ -7,8 +7,24 @@ module Db
         return unless defined? namespace
 
         namespace :db do
+          task :make_pgpass do
+            prod_conf = ActiveRecord::Base.configurations['production']
+
+            # This won't work correctly for the PostGIS adapter, but I'm not sure if we should call that out directly
+            if prod_conf['adapter'] == 'postgresql'
+              passfile = File.expand_path '~/.pgpass'
+              File.open(passfile, 'w', 0600) do |f|
+                f << prod_conf.values_at('host', 'port', 'database', 'username', 'password').join(':')
+              end
+
+              at_exit do
+                File.delete passfile
+              end
+            end
+          end
+
           desc 'clones a source database to a destination database'
-          task :clone, [:manual] => :environment do |t, args|
+          task :clone, [:manual] => [:environment, :make_pgpass] do |t, args|
             invoke_cli = !args[:manual].nil?
             Db::Clone::Base.clone! invoke_cli
           end
